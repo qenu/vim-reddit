0af67ffe3a442e0803ff2562d682236b021d0cf6
Josh Hartigan <joshhartigan99@gmail.com>
2015-02-10 07:14:19 +0000

    correct title display

--- plugin/vimreddit.py
+++ plugin/vimreddit.py
@@ -44,7 +44,7 @@ def vim_reddit(sub):
     vim.command('setlocal noswapfile')
     vim.command('setlocal buftype=nofile')
 
-    bufwrite('/r/programming (http://www.reddit.com/r/' + sub + ')')
+    bufwrite('/r/' + sub + ' ' + '(http://www.reddit.com/r/' + sub + ')')
     bufwrite('')
 
     items = json.loads(urllib2.urlopen(redditurl(sub)).read())
