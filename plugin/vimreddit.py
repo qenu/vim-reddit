a2d4e1ae7a680c5c8e12e4b185681db5fbdb6a8d
Doug Beney <work@dougie.io>
2017-12-19 18:25:23 -0500

    Untitled

--- plugin/vimreddit.py
+++ plugin/vimreddit.py
@@ -132,9 +132,11 @@ def vim_reddit_link(in_browser=False):
         # line they are trying to edit and use
         # some regex magic to find a URL.
         URL_REGEX =\
-            r'/(http|https)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/'
+            r'(http|https)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?'
 
         URLS_IN_LINE = re.search(URL_REGEX, line)
         if URLS_IN_LINE:
             URL_IN_LINE = URLS_IN_LINE[0]
             print("URL " + URL_IN_LINE)
+        else:
+            print("No URLS in line")
